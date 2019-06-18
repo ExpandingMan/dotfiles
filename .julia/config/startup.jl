300f7baa265ddf9b8b73050294d49222d08bfaae
ExpandingMan <savastio@protonmail.com>
2019-06-18 10:11:50 -0400

    added ssh config samples

--- .julia/config/startup.jl
+++ .julia/config/startup.jl
@@ -2,6 +2,7 @@ atreplinit() do repl
     try
         @eval using Revise
         @async Revise.wait_steal_repl_backend()
-    catch
+    catch e
+        @warn("unable to load Revise", e)
     end
 end
