3dbaec557aaecea686051bcb41c7d1b5d6358b67
ExpandingMan <savastio@protonmail.com>
2020-05-18 16:29:14 -0400

    slightly improved install script by allowing to specify multiple files by name

--- install.jl
+++ install.jl
@@ -6,13 +6,18 @@
     For now there's almost nothing here, but I may add to this in the future.
 ====================================================================================================#
 
-const NAMES = (nvim=".config/nvim/init.vim",
-               alacritty=".config/alacritty/alacritty.yml",
-               i3=".config/i3/config")
+const NAMES = (nvim=(".config/nvim/init.vim",),
+               alacritty=(".config/alacritty/alacritty.yml",),
+               ranger=(".config/ranger/rc.conf",
+                       ".config/ranger/scope.sh"),
+               i3=(".config/i3/config",)
+              )
 
-function install(fname::AbstractString, homedir::AbstractString=ENV["HOME"])
-    dest = joinpath(homedir, fname)
-    @info("installing $fname to $dest")
-    cp(joinpath(@__DIR__,fname), dest, force=true, follow_symlinks=true)
+function install(fnames, homedir::AbstractString=ENV["HOME"])
+    for fname ∈ fnames
+        dest = joinpath(homedir, fname)
+        @info("installing $fname to $dest")
+        cp(joinpath(@__DIR__,fname), dest, force=true, follow_symlinks=true)
+    end
 end
 install(name::Symbol, homedir::AbstractString=ENV["HOME"]) = install(NAMES[name], homedir)
